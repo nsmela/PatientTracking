9ef5555bd7019e9d73b0a289b6bf6a32243bc851
Nathan Smela <nsmela@hotmail.com>
2023-05-12 09:11:34 -0700

    new editor is operational

--- Data/Patient/Patient.cs
+++ Data/Patient/Patient.cs
@@ -5,7 +5,6 @@
         public string LastName { get; set; } = string.Empty;
         public DateTime? StartDate { get; set; } = new DateTime();
         public uint Status { get; set; } = 0; //started = 0, planning = 1, treating = 2, completed = 3;
-        public List<IPatientTask> Tasks { get; set; } = new List<IPatientTask>();
         public List<PatientTaskGroup> TaskGroups { get; set; } = new List<PatientTaskGroup>();
     }
 }
